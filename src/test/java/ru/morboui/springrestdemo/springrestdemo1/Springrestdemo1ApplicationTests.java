fbc43ab9cfa05b86b247771c366fb093058845de
Dmitry B <lbvf6000@gmail.com>
2021-10-14 12:59:53 +0300

    Initial commit

---
+++ src/test/java/ru/morboui/springrestdemo/springrestdemo1/Springrestdemo1ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.morboui.springrestdemo.springrestdemo1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springrestdemo1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
