09d3e9b4b604f100dddbeaa1c001af91e06d19f1
Dmitry B <lbvf6000@gmail.com>
2021-10-14 13:42:57 +0300

    add Vue.js

--- src/main/java/ru/morboui/springrestdemo/springrestdemo1/controller/MessageController.java
+++ src/main/java/ru/morboui/springrestdemo/springrestdemo1/controller/MessageController.java
@@ -1,6 +1,5 @@
 package ru.morboui.springrestdemo.springrestdemo1.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
